a0fbdeb12b9afdddc1b9612c635b66cdcb30b947
Hwang <hhf0204@126.com>
2020-10-16 15:13:02 +0800

    完成了数据报表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import AddGoods from '../components/goods/AddGoods.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -33,7 +34,8 @@ const router = new VueRouter({
                 { path: '/params', component: Params },
                 { path: '/goods', component: GoodsList },
                 { path: '/goods/addgoods', component: AddGoods },
-                { path: '/orders', component: Order }
+                { path: '/orders', component: Order },
+                { path: '/reports', component: Report }
             ]
         }
     ]
